1b827f0693f6cbc8ee73350514642e344078ea14
Oliech <railaoliech124@gmail.com>
2024-03-23 22:17:38 +0300

    add home

--- chamber/scripts/getDate.js
+++ chamber/scripts/getDate.js
@@ -1,8 +1,5 @@
-
 document.getElementById('year').textContent = new Date().getFullYear();
 document.getElementById('lastModified').textContent = "Last Modified: " + document.lastModified;
-
-
 
 const ham = document.querySelector('.navigation');
 const hbutton = document.querySelector('#menu')
@@ -11,22 +8,3 @@ hbutton.addEventListener('click', () => {
     ham.classList.toggle('show');
     hbutton.classList.toggle('show');
 });
-
-
-const mode = document.querySelector("#dark-mode");
-const main = document.querySelector("main");
-
-mode.addEventListener("click", () => {
-	if (mode.textContent.includes("🌑")) {
-		main.style.background = "#000";
-		main.style.color = "#fff";
-		mode.textContent = "🌞";
-	} else {
-		main.style.background = "#eee";
-		main.style.color = "#000";
-		mode.textContent = "🌑";
-	}
-});
-
-
-
